b3bd0f8c3e45bc8e29b26ab307789c4ba32b9510
sev-oren <stepanovaliza_2002@mail.ru>
2025-10-05 03:50:58 +0500

    Fix flake8 code style issues

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,9 +1,10 @@
-﻿import pytest
+from app import app
 import sys
 import os
 # Добавляем корневую директорию в Python path
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from app import app
+
+
 def test_hello_world():
     with app.test_client() as client:
         response = client.get('/')
