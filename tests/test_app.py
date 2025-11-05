b02720d81df4e1032ec7e177012429a0f0b54171
sev-oren <stepanovaliza_2002@mail.ru>
2025-11-05 18:58:08 +0500

    fix: simplify pipeline and tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,12 +1,14 @@
+﻿import pytest
 from app import app
-import sys
-import os
-# Добавляем корневую директорию в Python path
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
 
 def test_hello_world():
     with app.test_client() as client:
         response = client.get('/')
         assert response.status_code == 200
         assert b'Hello' in response.data
+
+def test_health_check():
+    with app.test_client() as client:
+        response = client.get('/health')
+        assert response.status_code == 200
+        assert b'OK' in response.data
